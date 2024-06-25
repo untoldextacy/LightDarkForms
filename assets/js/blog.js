6c922fb5881afd4a42edf30c3d8748e39d354461
untoldextacy <vanlarec1991@gmail.com>
2024-06-24 20:43:52 -0400

    Files for page

---
+++ assets/js/blog.js
@@ -0,0 +1,38 @@
+const mainEl = document.querySelector('main');
+
+const buildElement = function (type, text, parent) {
+  const tag = document.createElement(type);
+  tag.textContent = text;
+  parent.appendChild(tag);
+
+  return tag;
+};
+
+const handleEmpty = function () {
+  buildElement('h2', 'No Blog posts yet...', mainEl);
+  const a = buildElement('a', 'Enter your own submission here!', mainEl);
+
+  a.href = './index.html';
+};
+
+const renderBlogList = function () {
+  const blogs = readLocalStorage();
+
+  if (!blogs.length) {
+    handleEmpty();
+
+    return;
+  }
+
+  for (let blog of blogs) {
+    const article = buildElement('article', null, mainEl);
+
+    buildElement('h2', blog.title, article);
+    buildElement('blockquote', blog.content, article);
+    buildElement('p', `Posted by: ${blog.username}`, article);
+
+    article.classList.add('card');
+  }
+};
+
+renderBlogList();
---
+++ assets/js/logic.js
@@ -0,0 +1,57 @@
+const modeBtnEl = document.querySelector('#toggle');
+
+const applyMode = function (mode) {
+  let icon, circleColor;
+
+  if (mode === 'light') {
+    icon = '☀️'
+    circleColor = '#ffb100';
+  } else {
+    icon = '🌑';
+    circleColor = '#82ff59';
+  }
+
+  modeBtnEl.textContent = icon;
+
+  document.body.classList = mode;
+
+  document.documentElement.style.setProperty('--circle-color', circleColor);
+};
+
+const handleModeToggle = function () {
+  const mode = readMode();
+
+  let nextMode;
+
+  if (mode === 'light') {
+    nextMode = 'dark';
+  } else {
+    nextMode = 'light';
+  }
+
+  applyMode(nextMode);
+
+  saveMode(nextMode);
+};
+
+const readLocalStorage = function () {
+  const stringData = localStorage.getItem('blogs');
+
+  const data = JSON.parse(stringData);
+
+  return data || [];
+};
+
+const readMode = function () {
+  const mode = localStorage.getItem('mode') || 'dark';
+
+  return mode;
+};
+
+const saveMode = function (mode) {
+  localStorage.setItem('mode', mode);
+};
+
+applyMode(readMode());
+
+modeBtnEl.addEventListener('click', handleModeToggle);
